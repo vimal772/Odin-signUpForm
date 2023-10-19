0c61c9994b486ad3c5690f3122925eabd4bf1561
vimal772 <vimal115077286@gmail.com>
2023-10-19 19:07:31 +0530

    Final ui is finished

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ let checkPassword = () =>{
     const password = document.querySelector('#password');
     const rePassword = document.querySelector('#repassword');
 
-    if(password.textContent === rePassword.textContent){
-        return;
+    if(password.value == rePassword.value){
+        password.style.border = `1px solid #71a148`;
+        password.style.borderRadius = `5px`;
+        password.style.color = `#5D6063`;
+        rePassword.style.border = `1px solid #71a148`;
+        rePassword.style.borderRadius = `5px`;
+        rePassword.style.color = `#5D6063`;
     }else{
-        password.classList.add('invalid');
-        rePassword.classList.add('invalid');
+        password.style.border = `1px solid rgb(224,75,75)`;
+        password.style.borderRadius = `5px`;
+        password.style.color = `rgb(224,75,75)`;
+        rePassword.style.border = `1px solid rgb(224,75,75)`;
+        rePassword.style.borderRadius = `5px`;
+        rePassword.style.color = `rgb(224,75,75)`;
     }
 }
